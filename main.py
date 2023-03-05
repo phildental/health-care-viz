aeb43753d3a3ced91fab5f62a653ed03195019e2
Felipe Marques <felipemarques@Felipes-MacBook-Pro-2.local>
2023-03-04 17:47:03 -0800

    Added a CSV export method

--- main.py
+++ main.py
@@ -51,4 +51,5 @@ merged_df = get_countries_coordinates(metrics_df)
 # Merge Countries Coord. with metrics_df
 countriescoord_df = merged_df.merge(metrics_df, left_on='formatted', right_on='CountryName')
 
+#Export to csv
 countriescoord_df.to_csv("/Users/felipemarques/Documents/GitHub/health-care-viz/dataset.csv", index=False)
